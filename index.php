e215d8e5ddf0ae5d7d04cf69c9bf9151fdf264da
will <iamhandian@gmail.com>
2014-01-11 22:12:08 +0800

    git init

---
+++ index.php
@@ -0,0 +1,137 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="css/base.css">
+	<link rel="stylesheet" href="css/main.css">
+	<link rel="stylesheet" href="css/pure/pure-grid.css">
+</head>
+<body>
+	<div class="macbook">
+		<div class="macbook-air">
+			<div class="up">
+				<div class="screen">
+					<img src="img/macpaper.png" alt="">
+					<h1>MacBook Air</h1>
+					<div class="webcam">
+						<span></span>
+					</div>
+				</div>
+				<div class="screen-back">
+					<img src="img/apple-logo.png
+					" alt="">
+				</div>
+			</div>
+			<div class="bottom">
+				<div class="keyboard">
+					<div class="black"></div>
+					<div class="keys">
+						<div class="row row1">
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+						</div>
+						<div class="row row2">
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+							<span class="sp1"></span>
+						</div>
+						<div class="row row3">
+							<span class="sp1"></span>
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+						</div>
+						<div class="row row4">
+							<span class="sp2"></span>
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+							<span class="sp2"></span>
+						</div>
+						<div class="row row5">
+							<span class="sp3"></span>
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+							<span class="sp3"></span>
+						</div>
+						<div class="row row6">
+							<span></span>
+							<span></span>
+							<span></span>
+							<span class="sp4"></span>
+							<span class="sp5"></span>
+							<span class="sp4"></span>
+							<span></span>
+							<span></span>
+							<span></span>
+						</div>
+					</div>
+					<div class="touchpad"></div>
+				</div>
+				<div class="keyboard-back">
+
+					<div class="line pt20">
+						<div class="dot fl"></div>
+						<div class="dot fr"></div>
+					</div>
+					<div class="line pt50">
+						<div class="dot fl"></div>
+						<div class="dot fr"></div>
+					</div>
+					<div class="keyboard-wrap"></div>
+				</div>
+			</div>	
+		</div>
+		<div class="title">
+			<img src="img/title.png" height="30" width="282" alt="">
+		</div>
+		
+	</div>
+	<script src="js/jquery.js"></script>
+	<script src="js/main.js"></script>
+</body>
+</html>
